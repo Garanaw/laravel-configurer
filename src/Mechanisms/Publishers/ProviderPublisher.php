5ac20b1c8b7661f393d51225861a6e0f883ecdd3
Garanaw <garanaw@gmail.com>
2026-05-17 12:52:07 +0200

    Cleans up provider publisher

--- src/Mechanisms/Publishers/ProviderPublisher.php
+++ src/Mechanisms/Publishers/ProviderPublisher.php
@@ -6,22 +6,13 @@ namespace Garanaw\LaravelConfigurer\Mechanisms\Publishers;
 
 use Garanaw\LaravelConfigurer\Contracts\PublisherContract;
 use Garanaw\LaravelConfigurer\Library;
-use Illuminate\Console\OutputStyle;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Support\Facades\Process;
+
 use function Illuminate\Support\artisan_binary;
 use function Illuminate\Support\php_binary;
-use function Laravel\Prompts\alert;
 
 class ProviderPublisher implements PublisherContract
 {
-    public function __construct(
-        private readonly Application $app,
-        private readonly Kernel $artisan,
-        private readonly OutputStyle $output,
-    ) {}
-
     public function publish(Library $library): void
     {
         $provider = $library->publishCommands['provider'] ?? null;
@@ -37,21 +28,5 @@ class ProviderPublisher implements PublisherContract
             '--provider',
             $provider,
         ]);
-
-//        if (! class_exists($provider)) {
-//            alert(sprintf('The provider class %s does not exist. Skipping publishing for %s.', $provider, $library->name));
-//
-//            return;
-//        }
-
-//        $this->app->register($provider, force: true);
-//
-//        $params = ['--provider' => $provider];
-//
-//        $this->artisan->call(
-//            command: 'vendor:publish',
-//            parameters: $params,
-//            outputBuffer: $this->output,
-//        );
     }
 }
