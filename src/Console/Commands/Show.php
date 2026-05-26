3e81096ffcff8f14417e1c6dd527f12376804c76
Garanaw <garanaw@gmail.com>
2026-05-26 10:00:38 +0200

    Adds Show command

---
+++ src/Console/Commands/Show.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Garanaw\LaravelConfigurer\Console\Commands;
+
+use Illuminate\Config\Repository;
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Command;
+use Illuminate\Support\Enumerable;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+use function Laravel\Prompts\table;
+use function Laravel\Prompts\warning;
+
+#[AsCommand('configurer:show')]
+#[Description('Shows application configuration')]
+class Show extends Command
+{
+    protected $signature = 'configurer:show
+                            {--tags=* : Show the libraries that contain the given tags.}';
+
+    public function handle(
+        Repository $config
+    ): void {
+        $allLibraries = collect($config->get('configurer.libraries', []))
+            ->when(
+                $this->option('tags'),
+                fn (Enumerable $libraries) => $libraries->filter(
+                    fn (array $library) => collect($library['tags'] ?? [])->intersect($this->option('tags'))->isNotEmpty()
+                )
+            );
+
+        if ($allLibraries->isEmpty()) {
+            warning('No libraries found with the given tags.');
+
+            return;
+        }
+
+        table(
+            headers: ['Library', 'GitHub'],
+            rows: $allLibraries->map(
+                static function (array $library) {
+                    return [
+                        'Library' => $library['name'],
+                        'GitHub' => $library['github'],
+                    ];
+                }
+            )
+        );
+    }
+}
